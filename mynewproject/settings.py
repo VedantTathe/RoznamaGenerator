faadaefade5ae6e63e62b7b9cdcacb1ad8bda3d6
Vedant Tathe <tathevedant70@gmail.com>
2024-08-15 14:39:23 +0530

    final hosting changes

--- mynewproject/settings.py
+++ mynewproject/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = 'django-insecure-41@y-!%-*97=m0rgl^n&8^1h3h5r=9ymfzd1j$#6jhw7ox%fmk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+# ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app','.now.sh']
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME: 
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -119,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MY_ROOT = os.path.join(BASE_DIR,'main','static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
