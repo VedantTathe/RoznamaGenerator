3bcb71ca71691d92a020472852270b8381fd1018
Vedant Tathe <tathevedant70@gmail.com>
2024-08-15 14:42:22 +0530

    host1

--- mynewproject/settings.py
+++ mynewproject/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MY_ROOT = os.path.join(BASE_DIR,'main','static')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# MY_ROOT = os.path.join(BASE_DIR,'main','static')
 
 
 # Default primary key field type
